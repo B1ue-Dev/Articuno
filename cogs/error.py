12f8d3ab5d95ba857db60bbeefe721def64f7db9
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-02-02 10:32:55 +0700

    Rewrite error.py

--- cogs/error.py
+++ cogs/error.py
@@ -1,16 +1,4 @@
-'''
-
-This is a cog for error handler.
-This will define ``on_command_error`` and ``on_slash_command_error``.
-If there is an error during the execution of a command, it will send a traceback.
-
-'''
-
-
-
-
-
-
+from http.client import HTTPException
 import discord
 from discord.ext import commands
 import discord_slash
@@ -19,16 +7,23 @@ from datetime import datetime, timezone
 import traceback
 
 
+# Excuse me but I am lazy
+listener = commands.Cog.listener
+
 
 class Error(commands.Cog):
 	def __init__(self, bot: commands.Bot):
 		self.bot = bot
 
-	
-	@commands.Cog.listener()
-	async def on_command_error(self, ctx, error):
-		# No idea what I am doing here
+
+	'''
+	Handle ``on_slash_command_error`` event
+	'''
+
+	@listener()
+	async def on_slash_command_error(self, ctx: InteractionContext, error):
 		error_time: datetime = datetime.utcnow().replace(tzinfo=timezone.utc)
+
 		traceb2 = traceback.format_exception(type(error), error, error.__traceback__)
 		traceb = ''.join(traceb2)
 		traceb = traceb.replace('`','')
@@ -36,10 +31,153 @@ class Error(commands.Cog):
 		traceb = traceb.replace('\\t','\t')
 		traceb = traceb.replace('\\r','\r')
 		traceb = traceb.replace("\\","/")
-		# Format the messaege
-		embed = discord.Embed(title="An error occured", description=f"Command **{ctx.command}** triggered an error during execution")
+		er = ""
+		for i in traceb:
+			er = er + f"{i}"
+		embed=discord.Embed(title="**Uh oh...**",description="An error occurred. The developer team is dealing with the problem now.\nJoin the [**Support Server**](https://discord.gg/rQHRQ8JjSY) for more help.",color=discord.Color.red())
 		embed.add_field(name="Error",value=f'```py\n{type(error).__name__}: {error}\n```')
-		await ctx.send(embed=embed)
+		await ctx.send(embed=embed, hidden=True)
+
+		errorc2 = await self.bot.fetch_channel(867039086387789874)
+		try:
+			error=discord.Embed(title="An error occurred.",
+					description=f"Caused by **{ctx.command}**\n__Author:__ {ctx.author} `{ctx.author.id}`\n__Guild:__ {ctx.guild.name} `{ctx.guild.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+		except:
+			error=discord.Embed(title="An error occurred.",
+					description=f"Caused by **{ctx.command}**\n__Author ID:__ {ctx.author} `{ctx.author.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+		try:
+			error.add_field(name="Error",value=f"```py\n{traceb}\n```")
+			await errorc2.send(embed=error)
+		except Exception as e:
+			try:
+				error1=discord.Embed(title="An error occurred.",
+								description=f"Caused by **{ctx.command}**\n__Author:__ {ctx.author} `{ctx.author.id}`\n__Guild:__ {ctx.guild.name} `{ctx.guild.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+			except:
+				error1=discord.Embed(title="An error occurred.",
+								description=f"Caused by **{ctx.command}**\n__Author ID:__ {ctx.author} `{ctx.author.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+			error1.add_field(name="Error",value=f"```py\n...{er[-1000:]}\n```")
+			await errorc2.send(embed=error1)
+
+
+
+	'''
+	Handle ``on_component_callback_error`` event
+	'''
+
+	@listener()
+	async def on_component_callback_error(self, ctx: InteractionContext, error):
+		error_time: datetime = datetime.utcnow().replace(tzinfo=timezone.utc)
+
+		traceb2 = traceback.format_exception(type(error), error, error.__traceback__)
+		traceb = ''.join(traceb2)
+		traceb = traceb.replace('`','')
+		traceb = traceb.replace('\\n','\n')
+		traceb = traceb.replace('\\t','\t')
+		traceb = traceb.replace('\\r','\r')
+		traceb = traceb.replace("\\","/")
+		er = ""
+		for i in traceb:
+			er = er + f"{i}"
+		embed=discord.Embed(title="**Uh oh...**",description="An error occurred. The developer team is dealing with the problem now.\nJoin the [**Support Server**](https://discord.gg/rQHRQ8JjSY) for more help.",color=discord.Color.red())
+		embed.add_field(name="Error",value=f'```py\n{type(error).__name__}: {error}\n```')
+		try:
+			await ctx.send(embed=embed, hidden=True)
+		except:
+			pass
+
+		errorc2 = await self.bot.fetch_channel(867039086387789874)
+		try:
+			error=discord.Embed(title="An error occurred.",
+					description=f"Caused by **{ctx.command}**\n__Author:__ {ctx.author} `{ctx.author.id}`\n__Guild:__ {ctx.guild.name} `{ctx.guild.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+		except:
+			error=discord.Embed(title="An error occurred.",
+					description=f"Caused by **{ctx.command}**\n__Author ID:__ {ctx.author} `{ctx.author.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+		try:
+			error.add_field(name="Error",value=f"```py\n{traceb}\n```")
+			await errorc2.send(embed=error)
+		except Exception as e:
+			try:
+				error1=discord.Embed(title="An error occurred.",
+								description=f"Caused by **{ctx.command}**\n__Author:__ {ctx.author} `{ctx.author.id}`\n__Guild:__ {ctx.guild.name} `{ctx.guild.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+			except:
+				error1=discord.Embed(title="An error occurred.",
+								description=f"Caused by **{ctx.command}**\n__Author ID:__ {ctx.author} `{ctx.author.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+			error1.add_field(name="Error",value=f"```py\n...{er[-1000:]}\n```")
+			await errorc2.send(embed=error1)
+
+
+
+	'''
+	Handle ``on_command_error`` event
+	'''
+
+
+	@listener()
+	async def on_command_error(self, ctx, error):
+
+		# CommandNotFound
+		if isinstance(error, commands.errors.CommandNotFound):
+			return None
+
+		# CommandInvokeError
+		elif isinstance(error,commands.CommandInvokeError):
+			error_time: datetime = datetime.utcnow().replace(tzinfo=timezone.utc)
+
+			traceb2 = traceback.format_exception(type(error), error, error.__traceback__)
+			traceb = ''.join(traceb2)
+			traceb = traceb.replace('`','')
+			traceb = traceb.replace('\\n','\n')
+			traceb = traceb.replace('\\t','\t')
+			traceb = traceb.replace('\\r','\r')
+			traceb = traceb.replace("\\","/")
+			er = ""
+			for i in traceb:
+				er = er + f"{i}"
+			embed=discord.Embed(title="**Uh oh...**",description="An error occurred. The developer team is dealing with the problem now.\nJoin the [**Support Server**](https://discord.gg/rQHRQ8JjSY) for more help.",color=discord.Color.red())
+			embed.add_field(name="Error",value=f'```py\n{type(error).__name__}: {error}\n```')
+			await ctx.send(embed=embed, hidden=True)
+
+			errorc2 = await self.bot.fetch_channel(867039086387789874)
+			try:
+				error=discord.Embed(title="An error occurred.",
+						description=f"Caused by **{ctx.command}**\n__Author:__ {ctx.author} `{ctx.author.id}`\n__Guild:__ {ctx.guild.name} `{ctx.guild.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+			except:
+				error=discord.Embed(title="An error occurred.",
+						description=f"Caused by **{ctx.command}**\n__Author ID:__ {ctx.author} `{ctx.author.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+			try:
+				error.add_field(name="Error",value=f"```py\n{traceb}\n```")
+				await errorc2.send(embed=error)
+			except Exception as e:
+				try:
+					error1=discord.Embed(title="An error occurred.",
+									description=f"Caused by **{ctx.command}**\n__Author:__ {ctx.author} `{ctx.author.id}`\n__Guild:__ {ctx.guild.name} `{ctx.guild.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+				except:
+					error1=discord.Embed(title="An error occurred.",
+									description=f"Caused by **{ctx.command}**\n__Author ID:__ {ctx.author} `{ctx.author.id}`\n__Occured:__ <t:{error_time.timestamp():.0f}:R>",color=discord.Color.red())
+				error1.add_field(name="Error",value=f"```py\n...{er[-1000:]}\n```")
+				await errorc2.send(embed=error1)
+
+		# MissingRequiredArgument
+		elif isinstance(error, commands.errors.MissingRequiredArgument):
+			await ctx.send(f"{error}")
+
+		# BadUnionArgument
+		elif isinstance(error, commands.errors.BadUnionArgument):
+			await ctx.send(f"{error}")
+
+		# BadArgument
+		elif isinstance(error,commands.BadArgument):
+			await ctx.send(f"{error}")
+
+		# EmojiNotFound
+		elif isinstance(error, commands.EmojiNotFound):
+			await ctx.send(f"{error}")
+		
+		# NotOwner
+		elif isinstance(error, commands.NotOwner):
+			await ctx.send("No!")
+
+
 
 
 
