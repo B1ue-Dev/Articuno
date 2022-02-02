331c77bfceed16c45c9c845779a3fba73c32d45f
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-02-02 10:48:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ import jishaku
 import json
 
 
-intents = discord.Intents.default()
-intents.members = True
-# Activity for bot
-activity = discord.Activity(type=discord.ActivityType.watching, name="for /")
-# ...
-bot = commands.Bot(command_prefix="!",
-		intents=intents,
-		activity=activity)
-slash = SlashCommand(bot, sync_commands = True, override_type = True, delete_from_unused_guilds = True)
-
-
 
 # Token
 with open('./data/config.json') as bot_data:
-    data = json.load(bot_data)
+	data = json.load(bot_data)
+
+
+
+
+# 3 dots here
+intents = discord.Intents.default()
+intents.members = True
+activity = discord.Activity(type=discord.ActivityType.watching,
+							name="for v3.0 /help")
+bot = commands.Bot(command_prefix="$",
+					intents=intents,
+					activity=activity,
+					enable_debug_events=True)
+slash = SlashCommand(bot, sync_commands = True, override_type = True, delete_from_unused_guilds = True)
+
 
 
 
@@ -29,11 +33,12 @@ bot.load_extension('jishaku') # Jishaku is for debug
 bot.load_extension('cogs.admin')
 bot.load_extension('cogs.automod')
 bot.load_extension('cogs.basic')
-'''
+bot.load_extension('cogs.error')
 bot.load_extension('cogs.fun')
-bot.load_extension('cogs.logs')
+bot.load_extension('cogs.hacktool')
+bot.load_extension('cogs.menus')
 bot.load_extension('cogs.server')
-'''
+
 
 
 
@@ -44,6 +49,9 @@ async def on_ready():
 	print('Connected to: {}'.format(bot.user.name))
 	print('Bot ID: {}'.format(bot.user.id))
 	print(f'Latency: {websocket}ms')
+
+
+
 
 
 #bot.run
