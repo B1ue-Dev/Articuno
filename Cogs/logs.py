713a472e28bfd906d22d5a9372f92aa7ad055ba2
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-04-09 00:20:45 +0700

    Add on_message_create check

--- Cogs/logs.py
+++ Cogs/logs.py
@@ -1,13 +1,29 @@
 import interactions
-from interactions import *
 from interactions import extension_listener as listener
 
 
 class Logs(interactions.Extension):
-    def __init__(self, bot):
-        self.bot = bot
-    
+	def __init__(self, bot):
+		self.bot = bot
+	
 
-    @listener
-    async def on_message_create(self, message):
-        await ctx.send(f"{message.content}")
+	@listener(name="on_message_create")
+	async def message_create(self, message: interactions.Message):
+		channel = await message.get_channel()
+		if message.content == "$ping":
+			await channel.send("Pong!")
+	
+
+	@listener(name="on_message_delete")
+	async def message_delete(self, message: interactions.Message):
+		guild = await message.get_guild()
+		channel = await message.get_channel()
+		channels = await guild.get_all_channels()
+		logs = [ch._json for ch in await guild.get_all_channels() if int(ch.name) == "logs"]
+		await channel.send(f"```{logs}```")
+
+
+
+
+def setup(bot):
+	Logs(bot)
