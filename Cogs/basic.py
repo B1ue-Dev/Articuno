755074954aa6c2963197e0e642c5a965fa5e955f
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-04-08 09:55:59 +0700

    Initial commit

--- Cogs/basic.py
+++ Cogs/basic.py
@@ -1,11 +1,7 @@
 import interactions
 from interactions import extension_command as command
 from interactions import CommandContext
-import platform, psutil, datetime, json, utils.utils as utils, json
-
-with open("./data/servers.json") as f:
-	data = json.load(f)
-	ids = data['ID']
+import platform, psutil, datetime, json, utils.utils as utils
 
 
 load_time = datetime.datetime.now()
@@ -21,11 +17,8 @@ class Basic(interactions.Extension):
 		description="Ping Articuno"
 	)
 	async def ping(self, ctx: CommandContext):
-		if int(ctx.guild_id) in ids:
-			return
-		else:
-			websocket = f"{self.bot.latency * 1:.0f}"
-			await ctx.send(f"Pong! {websocket}ms")
+		websocket = f"{self.bot.latency * 1:.0f}"
+		await ctx.send(f"Pong! {websocket}ms")
 	
 
 	@command(
@@ -33,37 +26,34 @@ class Basic(interactions.Extension):
 		description="Get Articuno's stats"
 	)
 	async def stat(self, ctx: CommandContext):
-		if int(ctx.guild_id) in ids:
-			return
-		else:
-			proc = psutil.Process()
-			mems = proc.memory_full_info()
-			cpus = psutil.cpu_percent()
-			thread_counts = proc.num_threads()
-			mem = f"{utils.natural_size(mems.rss)}\n{utils.natural_size(mems.vms)}"
-			cpu = f"{cpus}%\n{thread_counts} Threads"
-			latency = f"{self.bot.latency * 1:.0f}ms"
-			python = platform.python_version()
-			os = str(platform.platform())
-			uptime = utils.pretty_date(load_time)
-			fields = [
-				interactions.EmbedField(name="Latency", value=latency, inline=True),
-				interactions.EmbedField(name="Python", value=python, inline=True),
-				interactions.EmbedField(name="CPU", value=cpu, inline=True),
-				interactions.EmbedField(name="Memory", value=mem, inline=True),
-				interactions.EmbedField(name="Uptime", value=uptime, inline=True),
-				interactions.EmbedField(name="System", value=os, inline=True)
-			]
-			url = f"https://cdn.discordapp.com/avatars/{int(self.bot.me.id)}/{self.bot.me.icon}"
-			thumbnail = interactions.EmbedImageStruct(url=url)._json
-			footer = interactions.EmbedFooter(text=f"Requested by {ctx.author.user.username}#{ctx.author.user.discriminator}", icon_url=f"{ctx.author.user.avatar_url}")
-			github = interactions.Button(
-				style = interactions.ButtonStyle.LINK,
-				label = "GitHub",
-				url = "https://github.com/Jimmy-Blue/Articuno"
-			)
-			embed = interactions.Embed(title="Articuno Stats", color=0x236adf, thumbnail=thumbnail, footer=footer, fields=fields)
-			await ctx.send(embeds=embed, components=[github])
+		proc = psutil.Process()
+		mems = proc.memory_full_info()
+		cpus = psutil.cpu_percent()
+		thread_counts = proc.num_threads()
+		mem = f"{utils.natural_size(mems.rss)}\n{utils.natural_size(mems.vms)}"
+		cpu = f"{cpus}%\n{thread_counts} Threads"
+		latency = f"{self.bot.latency * 1:.0f}ms"
+		python = platform.python_version()
+		os = str(platform.platform())
+		uptime = utils.pretty_date(load_time)
+		fields = [
+			interactions.EmbedField(name="Latency", value=latency, inline=True),
+			interactions.EmbedField(name="Python", value=python, inline=True),
+			interactions.EmbedField(name="CPU", value=cpu, inline=True),
+			interactions.EmbedField(name="Memory", value=mem, inline=True),
+			interactions.EmbedField(name="Uptime", value=uptime, inline=True),
+			interactions.EmbedField(name="System", value=os, inline=True)
+		]
+		url = f"https://cdn.discordapp.com/avatars/{int(self.bot.me.id)}/{self.bot.me.icon}"
+		thumbnail = interactions.EmbedImageStruct(url=url)._json
+		footer = interactions.EmbedFooter(text=f"Requested by {ctx.author.user.username}#{ctx.author.user.discriminator}", icon_url=f"{ctx.author.user.avatar_url}")
+		github = interactions.Button(
+			style = interactions.ButtonStyle.LINK,
+			label = "GitHub",
+			url = "https://github.com/Jimmy-Blue/Articuno"
+		)
+		embed = interactions.Embed(title="Articuno Stats", color=0x236adf, thumbnail=thumbnail, footer=footer, fields=fields)
+		await ctx.send(embeds=embed, components=[github])
 
 
 
