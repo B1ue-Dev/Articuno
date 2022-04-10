21e500bb7368421ab5c262fc3e236060abefbdb0
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-04-10 23:28:39 +0700

    fix!: Fixng the tag command and autocomplete

--- Cogs/tag.py
+++ Cogs/tag.py
@@ -39,11 +39,71 @@ class Tag(interactions.Extension):
 		)
 		await ctx.popup(modal)
 
+	
+	@interactions.extension_command(
+		name="tag_view",
+		description="View a tag (in development)",
+		scope=scope,
+		options = [
+			interactions.Option(
+				type=interactions.OptionType.STRING,
+				name="name",
+				description="The name of the tag you wish to view.",
+				required=True,
+				autocomplete=True,
+			)
+		]
+	)
+	async def view_tag(self, ctx: interactions.CommandContext, name: str):
+		guild_id = ctx.guild_id
+		tag = json.loads(open("./data/tag.json", "r").read())
+		if guild_id in tag:
+			if name in tag[guild_id]:
+				embed = interactions.Embed(
+					title=f"Tag: {name}",
+					description=tag[guild_id][name]["description"],
+				)
+				await ctx.send(embeds=embed)
+			else:
+				await ctx.send("Tag not found.")
+
 
 	@interactions.extension_modal(modal="new_tag")
 	async def new_tag(self, ctx: interactions.CommandContext, name: str, description: str):
-		embed = interactions.Embed(title=name, description=description)
-		await ctx.send(embeds=embed)
+		with open("./data/tag.json", "r") as f:
+			tags = json.load(f)
+			tags[name] = {
+				"description": description,
+			}
+		with open("./data/tag.json", "w") as f:
+			json.dump(tags, f, indent=4)
+		await ctx.send(f"Tag ``{name}`` created!")
+
+	@interactions.extension_autocomplete(
+		command=962728252584108092,
+		name="name",
+	)
+	async def tag_view(self, ctx: interactions.CommandContext, name: str = ""):
+		guild_id = ctx.guild_id
+		letters: list = list(name) if name != "" else []
+		tags = json.loads(open("./data/tag.json", "r").read())
+		if guild_id in tags:
+			if len(name) == 0:
+				await ctx.populate([interactions.Choice(name=tag[0], value=tag[0]) for tag in (
+					list(tags[guild_id].items())[0:24] if len(tags) > 25 else list(tags[guild_id].items())
+				)])
+			else:
+				choices: list = []
+				for tag in tags[guild_id]:
+					focus: str = "".join(letters)
+					if focus.lower() in tag.lower():
+						choices.append(interactions.Choice(name=tag, value=tag))
+				await ctx.populate(choices)
+		else:
+			return
+
+
+
 	
 
 
