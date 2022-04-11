5795550aabd183fe62c55b4912cb6eb4d656baa2
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-04-11 10:13:05 +0700

    feat: Adding a tag delete command

--- Cogs/tag.py
+++ Cogs/tag.py
@@ -11,12 +11,15 @@ class Tag(interactions.Extension):
 	def __init__(self, bot):
 		self.bot = bot
 
+	"""
+	Tag create: Create a tag.
+	"""
+
 	@interactions.extension_command(
-		name="tag",
+		name="tag_create",
 		description="Tag (in development)",
-		scope=scope
-	)
-	async def create_tag(self, ctx: interactions.CommandContext):
+		scope=scope)
+	async def tag_create(self, ctx: interactions.CommandContext):
 		modal = interactions.Modal(
 			title="Create new tag",
 			custom_id="new_tag",
@@ -39,7 +42,24 @@ class Tag(interactions.Extension):
 		)
 		await ctx.popup(modal)
 
-	
+	@interactions.extension_modal(modal="new_tag")
+	async def new_tag(self, ctx: interactions.CommandContext, name: str, description: str):
+		guild_id = ctx.guild_id
+		with open("./data/tag.json", "r") as f:
+			tags = json.load(f)
+			tags[guild_id][name] = {
+				"description": description,
+			}
+		with open("./data/tag.json", "w") as f:
+			json.dump(tags, f, indent=4)
+		await ctx.send(f"Tag ``{name}`` created!")
+
+
+
+	"""
+	Tag view: View a tag.
+	"""
+
 	@interactions.extension_command(
 		name="tag_view",
 		description="View a tag (in development)",
@@ -50,11 +70,8 @@ class Tag(interactions.Extension):
 				name="name",
 				description="The name of the tag you wish to view.",
 				required=True,
-				autocomplete=True,
-			)
-		]
-	)
-	async def view_tag(self, ctx: interactions.CommandContext, name: str):
+				autocomplete=True,)])
+	async def tag_view(self, ctx: interactions.CommandContext, name: str):
 		guild_id = ctx.guild_id
 		tag = json.loads(open("./data/tag.json", "r").read())
 		if guild_id in tag:
@@ -68,22 +85,124 @@ class Tag(interactions.Extension):
 				await ctx.send("Tag not found.")
 
 
-	@interactions.extension_modal(modal="new_tag")
-	async def new_tag(self, ctx: interactions.CommandContext, name: str, description: str):
+
+
+	"""
+	Tag edit: Edit a tag.
+	"""
+
+	@interactions.extension_command(
+		name="tag_edit",
+		description="Edit a tag (in development)",
+		scope=scope,
+		options = [
+			interactions.Option(
+				type=interactions.OptionType.STRING,
+				name="name",
+				description="The name of the tag you wish to edit.",
+				required=True,
+				autocomplete=True,)])
+	async def tag_edit(self, ctx: interactions.CommandContext, name: str):
+		guild_id = ctx.guild_id
+		tag = json.loads(open("./data/tag.json", "r").read())
+		if guild_id in tag:
+			if name in tag[guild_id]:
+				modal = interactions.Modal(
+					title=f"Edit tag: {name}",
+					custom_id="edit_tag",
+					components=[
+						interactions.TextInput(
+							style=interactions.TextStyleType.SHORT,
+							label="Name of the tag",
+							placeholder="Placeholder name",
+							custom_id="name",
+							max_length=100,
+							value=name,
+						),
+						interactions.TextInput(
+							style=interactions.TextStyleType.PARAGRAPH,
+							label="Description of the tag",
+							placeholder="Placeholder description",
+							custom_id="description",
+							max_length=2000,
+							value=tag[guild_id][name]["description"],
+						),
+					],
+				)
+				await ctx.popup(modal)
+			else:
+				await ctx.send("Tag not found.")
+
+	@interactions.extension_modal(modal="edit_tag")
+	async def edit_tag(self, ctx: interactions.CommandContext, name: str, description: str):
+		guild_id = ctx.guild_id
 		with open("./data/tag.json", "r") as f:
 			tags = json.load(f)
-			tags[name] = {
+			tags[guild_id][name] = {
 				"description": description,
 			}
 		with open("./data/tag.json", "w") as f:
 			json.dump(tags, f, indent=4)
-		await ctx.send(f"Tag ``{name}`` created!")
+		await ctx.send(f"Tag ``{name}`` edited!")
+
+
+
+	"""
+	Tag delete: Delete a tag.
+	"""
+	@interactions.extension_command(
+		name="tag_delete",
+		description="Delete a tag (in development)",
+		scope=scope,
+		options = [
+			interactions.Option(
+				type=interactions.OptionType.STRING,
+				name="name",
+				description="The name of the tag you wish to delete.",
+				required=True,
+				autocomplete=True,)])
+	async def tag_delete(self, ctx: interactions.CommandContext, name: str):
+		guild_id = ctx.guild_id
+		tag = json.loads(open("./data/tag.json", "r").read())
+		if guild_id in tag:
+			if name in tag[guild_id]:
+				del tag[guild_id][name]
+				with open("./data/tag.json", "w") as f:
+					json.dump(tag, f, indent=4)
+				await ctx.send(f"Tag ``{name}`` deleted!")
+			else:
+				await ctx.send("Tag not found.")
+
+
+
+	"""
+	Tag list: List all tags.
+	"""
+	@interactions.extension_command(
+		name="tag_list",
+		description="List all tags (in development)",
+		scope=scope)
+	async def tag_list(self, ctx: interactions.CommandContext):
+		guild_id = ctx.guild_id
+		tag = json.loads(open("./data/tag.json", "r").read())
+		if guild_id in tag:
+			embed = interactions.Embed(
+				title="Tags",
+				description="\n".join(tag[guild_id].keys()),
+			)
+			await ctx.send(embeds=embed)
+		else:
+			await ctx.send("No tags found.")
+
+
+	"""
+	Tag autocomplete.
+	"""
 
 	@interactions.extension_autocomplete(
-		command=962728252584108092,
-		name="name",
-	)
-	async def tag_view(self, ctx: interactions.CommandContext, name: str = ""):
+		command=962785667711139880,
+		name="name",)
+	async def view_tag(self, ctx: interactions.CommandContext, name: str = ""):
 		guild_id = ctx.guild_id
 		letters: list = list(name) if name != "" else []
 		tags = json.loads(open("./data/tag.json", "r").read())
@@ -102,6 +221,27 @@ class Tag(interactions.Extension):
 		else:
 			return
 
+	@interactions.extension_autocomplete(
+		command=962785666268266626,
+		name="name",)
+	async def edit_tag(self, ctx: interactions.CommandContext, name: str = ""):
+		guild_id = ctx.guild_id
+		letters: list = list(name) if name != "" else []
+		tags = json.loads(open("./data/tag.json", "r").read())
+		if guild_id in tags:
+			if len(name) == 0:
+				await ctx.populate([interactions.Choice(name=tag[0], value=tag[0]) for tag in (
+					list(tags[guild_id].items())[0:24] if len(tags) > 25 else list(tags[guild_id].items())
+				)])
+			else:
+				choices: list = []
+				for tag in tags[guild_id]:
+					focus: str = "".join(letters)
+					if focus.lower() in tag.lower():
+						choices.append(interactions.Choice(name=tag, value=tag))
+				await ctx.populate(choices)
+		else:
+			return
 
 
 	
