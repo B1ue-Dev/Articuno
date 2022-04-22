6471e8446202c0e2ef5b8f4fb49d5103ead1407b
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-04-23 00:56:11 +0700

    feat: A complete Pokedex command

--- Cogs/pokemon.py
+++ Cogs/pokemon.py
@@ -1,3 +1,4 @@
+from code import interact
 import interactions
 from interactions import extension_command as command
 import aiohttp, json, os, io
@@ -31,55 +32,83 @@ class Pokemon(EnhancedExtension):
 		self.bot = bot
 
 
-	@extension_command(
+	@command(
 		name="pokemon",
 		description="Show the information about a Pokemon",
-		scope=scope	
+		scope=scope,
+		options=[
+			interactions.Option(
+				type=interactions.OptionType.STRING,
+				name="name",
+				description="The name of the Pokemon",
+				required=True
+			)
+		]
 	)
-	@option(str, "name", "Name of the Pokemon")
 	async def pokemon(self, ctx: interactions.CommandContext,
 		name: str
 	):
+		name_lower = name.lower()
 		url = "https://some-random-api.ml/pokedex"
 		params = {
 			"pokemon": name
 		}
 		resp = await get_response(url, params)
-		with open("./data/pokemon.json") as f:
-			data = json.load(f)
-			name = name.lower()
-			if name in data:
-				name = data[name]['name']
-				num = data[name]['num']
-				desp = resp['description']
-				types = str(data[name]['types'])
-				types = types.replace("'", "")
-				types = types.replace("[", "")
-				types = types.replace("]", "")
-				hp = data[name]['baseStats']['hp']
-				atk = data[name]['baseStats']['atk']
-				defe = data[name]['baseStats']['def']
-				spa = data[name]['baseStats']['spa']
-				spd = data[name]['baseStats']['spd']
-				spe = data[name]['baseStats']['spe']
-				stats = "Hp: {}\nAtk: {}\nDef: {}\nSp.Atk: {}\nSp.Def: {}\nSpe: {}".format(hp, atk, defe, spa, spd, spe)
-				abilities = str(resp[abilities])
-				abilities = abilities.replace("'", "")
-				abilities = abilities.replace("[", "")
-				abilities = abilities.replace("]", "")
-				gen = int(resp[generation])
-		author = interactions.EmbedFooter(text=f"ID: {num} • First introduced in Generation {gen}")
-		fields = [
-			interactions.EmbedField(name="Stats", value=stats, inline=True),
-			interactions.EmbedField(name="Abilities", value=abilities, inline=True)
-		]
-		embed = interactions.Embed(
-			title=f"{name}",
-			description=f"{desp}",
-			author=author,
-			fields=fields
-		)
-		await ctx.send(embeds=embed)
+		desp = resp['description']
+		gen = resp['generation']
+		id = resp['id']
+		abilities = str(resp['abilities'])
+		abilities = abilities.replace("'", "")
+		abilities = abilities.replace("[", "")
+		abilities = abilities.replace("]", "")
+		img = resp['sprites']['animated']
+		evs = int(resp['family']['evolutionStage'])
+		if evs != 0:
+			evs = str(resp['family']['evolutionLine'])
+			evs = evs.replace("'", "")
+			evs = evs.replace("[", "")
+			evs = evs.replace("]", "")
+		else:
+			evs = None
+		height = resp['height']
+		weight = resp['weight']
+		data = json.loads(open("./data/pokemon.json", "r").read())
+		if name_lower in data:
+			name = data[name_lower]['name']
+			types = str(data[name_lower]['types'])
+			types = types.replace("'","")
+			types = types.replace("[","")
+			types = types.replace("]","")
+			hp = data[name_lower]['baseStats']['hp']
+			atk = data[name_lower]['baseStats']['atk']
+			defe = data[name_lower]['baseStats']['def']
+			spa = data[name_lower]['baseStats']['spa']
+			spd = data[name_lower]['baseStats']['spd']
+			spe = data[name_lower]['baseStats']['spe']
+			stats = "**HP:** {}\n**Attack:** {}\n**Defense:** {}\n**Special Attack:** {}\n**Special Defense:** {}\n**Speed:** {}".format(hp, atk, defe, spa, spd, spe)
+			gen = resp['generation']
+			egg_group = str(data[name_lower]['eggGroups'])
+			egg_group = egg_group.replace("'","")
+			egg_group = egg_group.replace("[","")
+			egg_group = egg_group.replace("]","")
+			footer = interactions.EmbedFooter(text="First introduced in Generation {}".format(gen), icon_url="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png")._json
+			thumbnail = interactions.EmbedImageStruct(url=img)._json
+			fields = [
+				interactions.EmbedField(name="Information", value=f"**Entry:** {id}\n**Type(s):** {types}\n**Abilities:** {abilities}\n**Egg Groups:** {egg_group}\n**Height:** {height}\n**Weight:** {weight}", inline=True),
+				interactions.EmbedField(name="Stats", value=stats, inline=True)
+			]
+			embed = interactions.Embed(
+				title=f"{name}",
+				description=f"{desp}",
+				footer=footer,
+				thumbnail=thumbnail,
+				fields=fields,
+			)
+			if evs is not None:
+				embed.add_field(name="Evolution Line", value=evs, inline=True)
+			await ctx.send(embeds=embed)
+		else:
+			await ctx.send("Pokemon not found.")
 
 
 
