9f48fadfb9bf180d75a076bf30cc0aa0d3f4258d
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-05-10 08:55:47 +0700

    feat: Moving back to interactions.py instead of enhanced

--- src/exts/pokemon.py
+++ src/exts/pokemon.py
@@ -1,13 +1,7 @@
 import interactions
 from interactions import extension_command as command
-import aiohttp, json, io
-from dotenv import load_dotenv
-from interactions.ext.enhanced import (
-	extension_command,
-	EnhancedExtension,
-	option,
-	ext_subcommand_base
-)
+import aiohttp, json, os, io
+
 
 
 async def get_response(url: str = None, params: dict = None):
@@ -24,21 +18,24 @@ async def get_response(url: str = None, params: dict = None):
 
 
 
-class Pokemon(EnhancedExtension):
+class Pokemon(interactions.Extension):
 	def __init__(self, bot) -> None:
 		super().__init__()
 		self.bot = bot
 
-	base = ext_subcommand_base("pokedex")
 
-	@base.subcommand(
-		name="search",
-		description="Show the information about a Pokemon"
-	)
-	@option(
-		type=interactions.OptionType.STRING,
-		name="pokemon_name",
-		autocomplete=True
+	@command(
+		name="pokedex",
+		description="Show the information about a Pokemon",
+		options=[
+			interactions.Option(
+				type=interactions.OptionType.STRING,
+				name="pokemon_name",
+				description="Pokemon name",
+				required=True,
+				autocomplete=True
+			)
+		]
 	)
 	async def _pokemon(self, ctx: interactions.CommandContext,
 		pokemon_name: str
@@ -49,86 +46,132 @@ class Pokemon(EnhancedExtension):
 			"pokemon": pokemon_name
 		}
 		resp = await get_response(url, params)
-		desp = resp['description']
-		gen = resp['generation']
-		id = resp['id']
-		abilities = str(resp['abilities'])
-		abilities = abilities.replace("'", "")
-		abilities = abilities.replace("[", "")
-		abilities = abilities.replace("]", "")
-		evs = int(resp['family']['evolutionStage'])
-		if evs != 0:
-			evs = str(resp['family']['evolutionLine'])
-			evs = evs.replace("'", "")
-			evs = evs.replace("[", "")
-			evs = evs.replace("]", "")
-		else:
-			evs = None
-		height = resp['height']
-		weight = resp['weight']
-		if int(id) < int(10):
-			id = "00" + str(id)
-			id = int(id)
-		elif int(id) < int(100):
-			id = "0" + str(id)
-			id = int(id)
-		else:
-			id = int(id)
-		sprites_url_still = f"https://www.serebii.net/art/th/{id}.png"
-		data = json.loads(open("./db/pokemon.json", "r").read())
-		if name_lower in data:
-			name = data[name_lower]['name']
-			types = str(data[name_lower]['types'])
-			types = types.replace("'","")
-			types = types.replace("[","")
-			types = types.replace("]","")
-			hp = data[name_lower]['baseStats']['hp']
-			atk = data[name_lower]['baseStats']['atk']
-			defe = data[name_lower]['baseStats']['def']
-			spa = data[name_lower]['baseStats']['spa']
-			spd = data[name_lower]['baseStats']['spd']
-			spe = data[name_lower]['baseStats']['spe']
-			stats = "**HP:** {}\n**Attack:** {}\n**Defense:** {}\n**Special Attack:** {}\n**Special Defense:** {}\n**Speed:** {}".format(hp, atk, defe, spa, spd, spe)
-			egg_group = str(data[name_lower]['eggGroups'])
-			egg_group = egg_group.replace("'","")
-			egg_group = egg_group.replace("[","")
-			egg_group = egg_group.replace("]","")
-			footer = interactions.EmbedFooter(text="First introduced in Generation {}".format(gen), icon_url="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png")
-			thumbnail = interactions.EmbedImageStruct(url=sprites_url_still)
-			fields = [
-				interactions.EmbedField(name="Information", value=f"**Entry:** {id}\n**Type(s):** {types}\n**Abilities:** {abilities}\n**Egg Groups:** {egg_group}\n**Height:** {height}\n**Weight:** {weight}", inline=True),
-				interactions.EmbedField(name="Stats", value=stats, inline=True)
-			]
-			embed = interactions.Embed(
-				title=f"{name}",
-				description=f"{desp}",
-				footer=footer,
-				thumbnail=thumbnail,
-				fields=fields,
-			)
-			if evs is not None:
-				embed.add_field(name="Evolution Line", value=evs, inline=True)
-			await ctx.send(embeds=embed)
-		else:
-			await ctx.send("Pokemon not found.")
+		try:
+			desp = resp['description']
+			gen = resp['generation']
+			id = resp['id']
+			abilities = str(resp['abilities'])
+			abilities = abilities.replace("'", "")
+			abilities = abilities.replace("[", "")
+			abilities = abilities.replace("]", "")
+			evs = int(resp['family']['evolutionStage'])
+			if evs != 0:
+				evs = str(resp['family']['evolutionLine'])
+				evs = evs.replace("'", "")
+				evs = evs.replace("[", "")
+				evs = evs.replace("]", "")
+			else:
+				evs = None
+			if int(id) < int(10):
+				id = "00" + str(id)
+				id = int(id)
+			elif int(id) < int(100):
+				id = "0" + str(id)
+				id = int(id)
+			else:
+				id = int(id)
+			sprites_url_still = f"https://www.serebii.net/art/th/{id}.png"
+			data = json.loads(open("./db/pokemon.json", "r", encoding="utf8").read())
+			if name_lower in data:
+				name = data[name_lower]['name']
+				types = str(data[name_lower]['types'])
+				types = types.replace("'","")
+				types = types.replace("[","")
+				types = types.replace("]","")
+				hp = data[name_lower]['baseStats']['hp']
+				atk = data[name_lower]['baseStats']['atk']
+				defe = data[name_lower]['baseStats']['def']
+				spa = data[name_lower]['baseStats']['spa']
+				spd = data[name_lower]['baseStats']['spd']
+				spe = data[name_lower]['baseStats']['spe']
+				stats = "**HP:** {}\n**Attack:** {}\n**Defense:** {}\n**Special Attack:** {}\n**Special Defense:** {}\n**Speed:** {}".format(hp, atk, defe, spa, spd, spe)
+				egg_group = str(data[name_lower]['eggGroups'])
+				egg_group = egg_group.replace("'","")
+				egg_group = egg_group.replace("[","")
+				egg_group = egg_group.replace("]","")
+				height = data[name_lower]['heightm']
+				weight = data[name_lower]['weightkg']
+
+				footer = interactions.EmbedFooter(text="First introduced in Generation {}".format(gen), icon_url="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png")
+				thumbnail = interactions.EmbedImageStruct(url=sprites_url_still)
+				fields = [
+					interactions.EmbedField(name="Information", value=f"**Entry:** {id}\n**Type(s):** {types}\n**Abilities:** {abilities}\n**Egg Groups:** {egg_group}\n**Height:** {height}\n**Weight:** {weight}", inline=True),
+					interactions.EmbedField(name="Stats", value=stats, inline=True)
+				]
+				embed = interactions.Embed(
+					title=f"{name}",
+					description=f"{desp}",
+					footer=footer,
+					thumbnail=thumbnail,
+					fields=fields,
+				)
+				if evs is not None:
+					embed.add_field(name="Evolution Line", value=evs, inline=True)
+				await ctx.send(embeds=embed)
+			else:
+				await ctx.send("Pokemon not found.")
+		except:
+			data = json.loads(open("./db/pokemon.json", "r", encoding="utf8").read())
+			if name_lower in data:
+				name = data[name_lower]['name']
+				id = data[name_lower]['num']	
+				types = str(data[name_lower]['types'])
+				types = types.replace("'","")
+				types = types.replace("[","")
+				types = types.replace("]","")
+				hp = data[name_lower]['baseStats']['hp']
+				atk = data[name_lower]['baseStats']['atk']
+				defe = data[name_lower]['baseStats']['def']
+				spa = data[name_lower]['baseStats']['spa']
+				spd = data[name_lower]['baseStats']['spd']
+				spe = data[name_lower]['baseStats']['spe']
+				stats = "**HP:** {}\n**Attack:** {}\n**Defense:** {}\n**Special Attack:** {}\n**Special Defense:** {}\n**Speed:** {}".format(hp, atk, defe, spa, spd, spe)
+				egg_group = str(data[name_lower]['eggGroups'])
+				egg_group = egg_group.replace("'","")
+				egg_group = egg_group.replace("[","")
+				egg_group = egg_group.replace("]","")
+				height = data[name_lower]['heightm']
+				weight = data[name_lower]['weightkg']
+				if int(id) < int(10):
+					id = "00" + str(id)
+					id = int(id)
+				elif int(id) < int(100):
+					id = "0" + str(id)
+					id = int(id)
+				else:
+					id = int(id)
+				sprites_url_still = f"https://www.serebii.net/art/th/{id}.png"
+
+				thumbnail = interactions.EmbedImageStruct(url=sprites_url_still)
+				fields = [
+					interactions.EmbedField(name="Information", value=f"**Entry:** {id}\n**Type(s):** {types}\n**Egg Groups:** {egg_group}\n**Height:** {height}\n**Weight:** {weight}", inline=True),
+					interactions.EmbedField(name="Stats", value=stats, inline=True)
+				]
+				embed = interactions.Embed(
+					title=f"{name}",
+					thumbnail=thumbnail,
+					fields=fields,
+				)
+				await ctx.send(embeds=embed)
 
 
-	base.finish()
-
-
-	@base.autocomplete("pokemon_name")
+	@interactions.extension_autocomplete(
+		command="pokedex",
+		name="pokemon_name"
+	)
 	async def auto_complete(self, ctx:interactions.CommandContext, pokemon_name: str = ""):
 		if pokemon_name != "":
 			letters: list = pokemon_name
 		else:
 			letters = []
-		data = json.loads(open("./db/pokemon.json", "r").read())
+		data = json.loads(open("./db/pokemon.json", "r", encoding="utf8").read())
 		if len(pokemon_name) == 0:
 			await ctx.populate(
 				[
 					interactions.Choice(
 						name=name[0].capitalize(), value=name[0].capitalize()) for name in (
-							list(data.items())[0:9] if len(data) > 10 else list(data.items())
+							list(data.items())[0:9] if len(data) > 10 else list(data.items()
+						)
 					)
 				]
 			)
