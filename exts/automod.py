0a1638071d9e2c8cce2fc1d96a432c5ac1ed3073
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-05-12 23:13:42 +0700

    fix: Fix variable assignment problem

--- exts/automod.py
+++ exts/automod.py
@@ -14,8 +14,8 @@ class Automod(interactions.Extension):
 
 	@listener(name="on_message_create")
 	async def _message_create(self, message: interactions.Message):
+		message_content = message.content.lower()
 		if message.guild_id == GUILD and message.guild_id is not None:
-			message_content = message.content.lower()
 
 			# If someone sends an invite link
 			if "discord.gg/" in message_content or "discordapp.com/invite" in message_content or "discord.com/invite" in message_content:
