d2706cd727781d1154f1854e438e4f7e336c42b3
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-05-13 10:57:33 +0700

    fix: Optimize

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from optparse import Option
 import interactions
 from interactions.ext import wait_for
 import os, datetime, logging, asyncio, json
@@ -5,6 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 bot_token = os.getenv("TOKEN")
 scope = int(os.getenv("SCOPE"))
+google_cloud = os.getenv("GOOGLE_CLOUD")
+google_cse = os.getenv("GOOGLE_CSE")
 
 #logging.basicConfig(level=logging.DEBUG)
 
@@ -41,10 +44,10 @@ bot.load('exts.pokemon')
 bot.load('exts.snipe')
 bot.load('exts.tag')
 
-bot.load('interactions.ext.enhanced')
+
+#bot.load('interactions.ext.enhanced')
 #bot.load('Cogs.get_method')
 #bot.load('test')
-
 
 
 
@@ -54,6 +57,19 @@ async def on_ready():
 	print(f'Logged in as {bot.me.name}')
 	print(f'ID: {bot.me.id}')
 	print(f'Latency: {websocket}ms')
+
+"""
+@bot.command(
+	name="ping",
+	description="Ping Blue",
+	scope=scope,
+)
+async def _ping(ctx: interactions.CommandContext):
+	await ctx.send("<@892080548342820925> Ping Blue.")
+	await ctx.send("<@&920717553616171088> Ping Developer.", allowed_mentions={"parse":["roles"]})
+"""
+
+
 
 
 
