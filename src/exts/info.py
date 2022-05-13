187f924012a84010ff6ca3e62e1134dc19278939
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-05-13 17:11:21 +0700

    feat: An information command, covering user, avatar and server

--- src/exts/info.py
+++ src/exts/info.py
@@ -59,29 +59,100 @@ class Info(interactions.Extension):
 	):
 		if sub_command == "user":
 			await self._info_user(ctx, user)
-
 		if sub_command == "avatar":
-			avatar = user.user.avatar_url
-			avatar_jpg = user.user.avatar_url[:-4] + ".jpg"
-			avatar_png = user.user.avatar_url[:-4] + ".png"
-			avatar_webp = user.user.avatar_url[:-4] + ".webp"
-			message = f"[JPG]({avatar_jpg})  [PNG]({avatar_png})  [WEBP]({avatar_webp})"
-			if avatar[:-4] == ".gif":
-				message += "  [GIF]" + "(" + avatar[:-4] + ".gif)"
-			embed = interactions.Embed(
-				title=f"{user.user.username}#{user.user.discriminator}",
-				description=f"{message}",
-				color=random.randint(0, 0xFFFFFF),
-				image=interactions.EmbedImageStruct(url=avatar),
-				footer=interactions.EmbedFooter(
-					text=f"Requested by {ctx.author.user.username}#{ctx.author.user.discriminator}",
-					icon_url=f"{ctx.author.user.avatar_url}"
-				)
-			)
-			await ctx.send(embeds=embed)
-
+			await self._info_avatar(ctx, user)
 		if sub_command == "server":
-			guild = interactions.Guild(**await self.bot._http.get_guild(ctx.guild_id), _client=self.bot._http)
+			await self._info_server(ctx)
+
+
+
+	async def _info_user(self, ctx: interactions.ComamndContext, user: str):
+		role = await (await ctx.get_guild()).get_role(role_id=user.roles[0])
+		name = user.user.username
+		discriminator = int(user.user.discriminator)
+		nick = user.nick
+		id = int(user.user.id)
+		joined_at = round(user.joined_at.timestamp())
+		created_at = user.user.id.epoch
+		avatar = user.user.avatar_url
+		highest_role_color = role.color
+		public_flags = user.user.public_flags
+		hypesquad = None
+		if isinstance(public_flags, int) is True:
+			if public_flags & 1 << 6:
+				hypesquad = "<:bravery:957684396268322886> Bravery"
+			elif public_flags & 1 << 7:
+				hypesquad = "<:brilliance:957684592498843658> Brilliance"
+			elif public_flags & 1 << 8:
+				hypesquad = "<:balance:957684753174241330> Balance"
+		else:
+			hypesquad = None
+		bot = user.user.bot
+		if bot is True:
+			bot = "Yes"
+		else:
+			bot = "No"
+		early_supporter = None
+		if isinstance(public_flags, int) is True:
+			if public_flags & 1 << 9:
+				early_supporter = "Yes"
+			else:
+				early_supporter = "No"
+		else:
+			early_supporter = "No"
+			fields = [
+			interactions.EmbedField(name="Name", value=f"{name}", inline=True),
+			interactions.EmbedField(name="Nickname", value=f"{nick}", inline=True),
+			interactions.EmbedField(name="ID", value=f"{id}", inline=True),
+			interactions.EmbedField(name="Joined at", value=f"<t:{joined_at}>", inline=True),
+			interactions.EmbedField(name="Created on", value=f"<t:{created_at}>", inline=True),
+			interactions.EmbedField(name="Highest role", value=f"{role.mention}", inline=True),
+			interactions.EmbedField(name="Hypesquad", value=f"{hypesquad}", inline=True),
+			interactions.EmbedField(name="Bot", value=f"{bot}", inline=True),
+			interactions.EmbedField(name="Early supporter", value=f"{early_supporter}", inline=True),
+		]
+		thumbnail = interactions.EmbedImageStruct(url=avatar)
+		footer = interactions.EmbedFooter(
+			text=f"Requested by {ctx.author.user.username}#{ctx.author.user.discriminator}",
+			icon_url=f"{ctx.author.user.avatar_url}"
+		)
+		embed = interactions.Embed(
+			title=f"{name}#{discriminator}",
+			color=highest_role_color,
+			thumbnail=thumbnail,
+			footer=footer,
+			fields=fields
+		)
+
+		await ctx.send(embeds=embed)
+
+
+
+	async def _info_avatar(self, ctx: interactions.CommandContext, user: str):
+		avatar = user.user.avatar_url
+		avatar_jpg = user.user.avatar_url[:-4] + ".jpg"
+		avatar_png = user.user.avatar_url[:-4] + ".png"
+		avatar_webp = user.user.avatar_url[:-4] + ".webp"
+		message = f"[JPG]({avatar_jpg})  [PNG]({avatar_png})  [WEBP]({avatar_webp})"
+		if avatar[:-4] == ".gif":
+			message += "  [GIF]" + "(" + avatar[:-4] + ".gif)"
+		embed = interactions.Embed(
+			title=f"{user.user.username}#{user.user.discriminator}",
+			description=f"{message}",
+			color=random.randint(0, 0xFFFFFF),
+			image=interactions.EmbedImageStruct(url=avatar),
+			footer=interactions.EmbedFooter(
+				text=f"Requested by {ctx.author.user.username}#{ctx.author.user.discriminator}",
+				icon_url=f"{ctx.author.user.avatar_url}"
+			)
+		)
+
+		await ctx.send(embeds=embed)
+
+
+
+	async def _info_server(self, ctx: interactions.CommandContext):
+			guild = interactions.Guild(**await self.bot._http.get_guild(int(ctx.guild_id)), _client=self.bot._http)
 			user = interactions.User(**await self.bot._http.get_user(int(guild.owner_id)), _client=self.bot._http)
 
 			name = guild.name
@@ -163,69 +234,6 @@ class Info(interactions.Extension):
 			if banner_bool is True and guild.banner_url is not None:
 				embed.add_field(name="Banner URL", value=f"[Banner_url]({guild.banner_url})", inline=True)
 			await ctx.send(embeds=embed)
-
-
-
-	async def _info_user(self, ctx: interactions.ComamndContext, user: str):
-		role = await (await ctx.get_guild()).get_role(role_id=user.roles[0])
-		name = user.user.username
-		discriminator = int(user.user.discriminator)
-		nick = user.nick
-		id = int(user.user.id)
-		joined_at = round(user.joined_at.timestamp())
-		created_at = user.user.id.epoch
-		avatar = user.user.avatar_url
-		highest_role_color = role.color
-		public_flags = user.user.public_flags
-		hypesquad = None
-		if isinstance(public_flags, int) is True:
-			if public_flags & 1 << 6:
-				hypesquad = "<:bravery:957684396268322886> Bravery"
-			elif public_flags & 1 << 7:
-				hypesquad = "<:brilliance:957684592498843658> Brilliance"
-			elif public_flags & 1 << 8:
-				hypesquad = "<:balance:957684753174241330> Balance"
-		else:
-			hypesquad = None
-		bot = user.user.bot
-		if bot is True:
-			bot = "Yes"
-		else:
-			bot = "No"
-		early_supporter = None
-		if isinstance(public_flags, int) is True:
-			if public_flags & 1 << 9:
-				early_supporter = "Yes"
-			else:
-				early_supporter = "No"
-		else:
-			early_supporter = "No"
-			fields = [
-			interactions.EmbedField(name="Name", value=f"{name}", inline=True),
-			interactions.EmbedField(name="Nickname", value=f"{nick}", inline=True),
-			interactions.EmbedField(name="ID", value=f"{id}", inline=True),
-			interactions.EmbedField(name="Joined at", value=f"<t:{joined_at}>", inline=True),
-			interactions.EmbedField(name="Created on", value=f"<t:{created_at}>", inline=True),
-			interactions.EmbedField(name="Highest role", value=f"{role.mention}", inline=True),
-			interactions.EmbedField(name="Hypesquad", value=f"{hypesquad}", inline=True),
-			interactions.EmbedField(name="Bot", value=f"{bot}", inline=True),
-			interactions.EmbedField(name="Early supporter", value=f"{early_supporter}", inline=True),
-		]
-		thumbnail = interactions.EmbedImageStruct(url=avatar)
-		footer = interactions.EmbedFooter(
-			text=f"Requested by {ctx.author.user.username}#{ctx.author.user.discriminator}",
-			icon_url=f"{ctx.author.user.avatar_url}"
-		)
-		embed = interactions.Embed(
-			title=f"{name}#{discriminator}",
-			color=highest_role_color,
-			thumbnail=thumbnail,
-			footer=footer,
-			fields=fields
-		)
-
-		await ctx.send(embeds=embed)
-
 
 
 
