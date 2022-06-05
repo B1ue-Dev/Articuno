032243f3827a1411d3028dc2b64fe4c7195a5993
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-06-05 16:28:43 +0000

    fix: Ping bot in main guild does not reply and replace ID

--- exts/automod.py
+++ exts/automod.py
@@ -4,15 +4,14 @@ import urllib.request, re
 
 
 class Automod(interactions.Extension):
-	def __init__(self, bot) -> None:
-		super().__init__()
+	def __init__(self, bot):
 		self.bot = bot
 
 
 	@listener(name="on_message_create")
 	async def _message_create(self, message: interactions.Message):
 		message_content = str(message.content.lower())
-		if int(message.guild_id) == 738938246574374913 and int(message.guild_id) is not None:
+		if int(message.guild_id) == 859030372783751168 and int(message.guild_id) is not None:
 
 			# If someone sends an invite link
 			DISCORD_INVITE = r"(?:(?:discord.?(?:gg|io|me|li)|discord(?:app)?.?com[/\\]{1,}(?:invite))(?:[\S]+)?[/\\]{1,}([^\s/]+?)(?=\b))|discord://-/invite/([^\s/]+?)(?=\b)"
@@ -41,6 +40,16 @@ class Automod(interactions.Extension):
 						await message.delete()
 						await channel.send(f"{message.member.mention}, please do not send a scam link.")
 						return
+
+			# If someone mentions the bot
+			if f"@{self.bot.me.id}" in message_content or f"<@{self.bot.me.id}>" in message_content:
+				channel = await message.get_channel()
+				embed = interactions.Embed(
+					title="It seems like you mentioned me",
+					description=f"I could not help much but noticed you mentioned me. You can type ``/`` and choose **{self.bot.me.name}** to see a list of available commands.",
+					color=0x6aa4c1
+				)
+				await channel.send(embeds=embed)
 
 		else:
 			# If someone mentions the bot
