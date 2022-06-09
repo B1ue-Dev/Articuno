e72b72be172ee996e40ad8dd7f0a06ce190b2db7
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-06-09 02:52:14 +0000

    chore: Update const.py

--- const.py
+++ const.py
@@ -14,11 +14,15 @@ global APIKEY
 global AUTHORIZATION
 global GOOGLE_CLOUD
 global GOOGLE_CSE
+global U_KEY
+global U_SECRET
 
 TOKEN = os.getenv('TOKEN')
-VERSION = "v4.1.1"
+VERSION = "v4.1.2"
 
 APIKEY = os.getenv("APIKEY")
 AUTHORIZATION = os.getenv("AUTHORIZATION")
 GOOGLE_CLOUD = os.getenv("GOOGLE_CLOUD")
 GOOGLE_CSE = os.getenv("GOOGLE_CSE")
+U_KEY = os.getenv('UBERDUCK_KEY')
+U_SECRET = os.getenv('UBERDUCK_SECRET')
