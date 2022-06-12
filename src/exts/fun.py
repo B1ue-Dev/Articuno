d29a3c007e4af6cfc5455072e258f5a45c7134ab
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-06-12 15:48:29 +0700

    style: Code cleanup

--- src/exts/fun.py
+++ src/exts/fun.py
@@ -11,17 +11,7 @@ import io
 import datetime
 import interactions
 import aiohttp
-
-
-async def get_response(url: str = None, params: dict = None):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as resp:
-            if resp.status == 200:
-                if resp.content_type == "application/json":
-                    return await resp.json()
-                elif resp.content_type in {"image/png", "image/jpeg", "image/gif"}:
-                    return io.BytesIO(await resp.read())
-    await session.close()
+from utils.utils import get_response
 
 
 class Fun(interactions.Extension):
