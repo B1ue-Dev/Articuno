54ca9e99abe1856957ef2cfdd754637514a1005a
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-09-02 16:57:23 +0700

    refactor: Use listdir for Extension list

--- const.py
+++ const.py
@@ -6,6 +6,7 @@ For value data.
 
 import os
 import dotenv
+
 dotenv.load_dotenv()
 
 global TOKEN
@@ -21,15 +22,31 @@ global GOOGLE_CSE
 global U_KEY
 global U_SECRET
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv("TOKEN")
 VERSION = "v4.2.0a"
-EXT_CORE = [file.replace(".py", "") for file in os.listdir("exts/core") if not file.startswith("_")]
-EXT_FUN = [file.replace(".py", "") for file in os.listdir("exts/fun") if not file.startswith("_")]
-EXT_SERVER = [file.replace(".py", "") for file in os.listdir("exts/server") if not file.startswith("_")]
-EXT_UTILS = [file.replace(".py", "") for file in os.listdir("exts/utils") if not file.startswith("_")]
+EXT_CORE = [
+    file.replace(".py", "")
+    for file in os.listdir("exts/core")
+    if not file.startswith("_")
+]
+EXT_FUN = [
+    file.replace(".py", "")
+    for file in os.listdir("exts/fun")
+    if not file.startswith("_")
+]
+EXT_SERVER = [
+    file.replace(".py", "")
+    for file in os.listdir("exts/server")
+    if not file.startswith("_")
+]
+EXT_UTILS = [
+    file.replace(".py", "")
+    for file in os.listdir("exts/utils")
+    if not file.startswith("_")
+]
 APIKEY = os.getenv("APIKEY")
 AUTHORIZATION = os.getenv("AUTHORIZATION")
 GOOGLE_CLOUD = os.getenv("GOOGLE_CLOUD")
 GOOGLE_CSE = os.getenv("GOOGLE_CSE")
-U_KEY = os.getenv('UBERDUCK_KEY')
-U_SECRET = os.getenv('UBERDUCK_SECRET')
+U_KEY = os.getenv("UBERDUCK_KEY")
+U_SECRET = os.getenv("UBERDUCK_SECRET")
