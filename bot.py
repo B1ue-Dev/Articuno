6944963235a8e14114fa22897e1c6beb14cc40da
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-09-02 22:51:35 +0700

    refactor!: Change how Extensions are loaded

--- bot.py
+++ bot.py
@@ -8,17 +8,13 @@ import logging
 import datetime
 import interactions
 from interactions.ext.wait_for import setup
-from const import TOKEN, VERSION, EXT_CORE
-from status import WebSocketClient
+from const import TOKEN, VERSION
 
 # logging.basicConfig(level=logging.DEBUG)
 
 client = interactions.Client(
     token=TOKEN,
-    intents=interactions.Intents.DEFAULT
-    | interactions.Intents.GUILD_MEMBERS
-    | interactions.Intents.GUILD_MESSAGE_CONTENT
-    | interactions.Intents.GUILD_PRESENCES,
+    intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MESSAGE_CONTENT,
     presence=interactions.ClientPresence(
         activities=[
             interactions.PresenceActivity(
@@ -27,22 +23,28 @@ client = interactions.Client(
         ],
         status=interactions.StatusType.ONLINE,
     ),
+    disable_sync=True,
 )
-client._websocket = WebSocketClient(TOKEN, client._intents)
 setup(client)
 client.load("interactions.ext.files")
 client.load("utils.cache")
-
-# [client.load(f"exts.core.{ext}") for ext in EXT_CORE]
-client.load("exts.fun.whos_that_pokemon")
+client.load("utils.error")
+client.load("exts.core.__init__")
+client.load("exts.server.__init__")
+client.load("exts.fun.__init__")
+client.load("exts.utils.__init__")
 
 
 @client.event
 async def on_ready():
     """Fires up READY"""
     websocket = f"{client.latency * 1:.0f}"
-    log_time = (datetime.datetime.now() + datetime.timedelta(hours=7)).strftime("%d/%m/%Y %H:%M:%S")
-    logging.debug("""[%s] Logged in as %s. Latency: %sms.""", log_time, client.me.name, websocket)
+    log_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=7)).strftime(
+        "%d/%m/%Y %H:%M:%S"
+    )
+    logging.debug(
+        """[%s] Logged in as %s. Latency: %sms.""", log_time, client.me.name, websocket
+    )
     print(f"""[{log_time}] Logged in as {client.me.name}. Latency: {websocket}ms.""")
 
 
