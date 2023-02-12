9daa1532dc888eb5a9c94264ef96299b110cd606
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2023-02-13 02:22:57 +0700

    feat: Migrate /eval to v5

--- exts/core/eval.py
+++ exts/core/eval.py
@@ -7,17 +7,19 @@ Eval command.
 import logging
 import datetime
 import io
+import string
+import re
 import textwrap
 import inspect
 import contextlib
 import traceback
 import asyncio
-from typing import List
 import interactions
-from interactions.ext.paginator import Page, Paginator
+from interactions.ext.paginators import Paginator
+from interactions.ext.prefixed_commands import prefixed_command, PrefixedContext
 
 
-def page_paginator(text: str) -> List[str]:
+def page_paginator(text: str) -> list[str]:
     """
     This function takes a string and splits it into chunks of 1000 characters.
 
@@ -27,16 +29,28 @@ def page_paginator(text: str) -> List[str]:
     :rtype: list
     """
 
-    last = 0
+    words = [
+        re.sub("^[{0}]+|[{0}]+$".format(string.punctuation), "", w)
+        for w in text.split()
+    ]
     pages = []
-    for curr in range(0, len(text)):
-        if curr % 1000 == 0:
-            pages.append(text[last:curr])
-            last = curr
-            appd_index = curr
-    if appd_index != len(text) - 1:
-        pages.append(text[last:curr])
-    return list(filter(lambda a: a != "", pages))
+    s = ""
+    for i in range(len(words)):
+        if len(s) < 1000:
+            if i == len(words) - 1:
+                s += f"{words[i]} "
+                pages.append(s)
+            else:
+                s += f"{words[i]} "
+        else:
+            pages.append(s)
+            s = ""
+            if i == len(words) - 1:
+                s += f"\n{words[i]} "
+                pages.append(s)
+            else:
+                s += f"\n{words[i]} "
+    return pages
 
 
 class Eval(interactions.Extension):
@@ -45,11 +59,11 @@ class Eval(interactions.Extension):
     def __init__(self, client: interactions.Client) -> None:
         self.client: interactions.Client = client
 
-    @interactions.extension_command(
+    @interactions.slash_command(
         name="eval",
         description="Evaluates some code.",
         options=[
-            interactions.Option(
+            interactions.SlashCommandOption(
                 type=interactions.OptionType.STRING,
                 name="code",
                 description="Code to evaluate.",
@@ -57,7 +71,7 @@ class Eval(interactions.Extension):
             )
         ],
     )
-    async def _eval(self, ctx: interactions.CommandContext, code: str):
+    async def eval(self, ctx: interactions.InteractionContext, code: str) -> None:
         """Evaluates some code."""
 
         if int(ctx.user.id) != 892080548342820925:
@@ -69,10 +83,10 @@ class Eval(interactions.Extension):
 
         env = {
             "ctx": ctx,
-            "channel": await ctx.get_channel(),
+            "channel": ctx.channel,
             "author": ctx.author,
             "user": ctx.user,
-            "guild": await ctx.get_guild(),
+            "guild": ctx.guild,
             "message": ctx.message,
             "source": inspect.getsource,
             "interactions": interactions,
@@ -105,85 +119,84 @@ class Eval(interactions.Extension):
                 out_pages = page_paginator(value)
                 pag_pages = []
                 for page in out_pages:
-                    pag_pages.append(Page(f"```py\n{page}```"))
-                await Paginator(
-                    client=self.client,
-                    ctx=ctx,
-                    pages=pag_pages,
-                    timeout=12,
-                    use_select=False,
-                    remove_after_timeout=True,
-                ).run()
+                    pag_pages.append(f"```py\n{page}\n```")
 
+                paginator = Paginator.create_from_list(
+                    self.client,
+                    content=pag_pages,
+                    page_size=2000,
+                )
+
+                await paginator.send(ctx)
             else:
                 await ctx.send("```py\nNone\n```", ephemeral=True)
 
-    @interactions.extension_listener(name="on_message_create")
-    async def _message_create(self, message: interactions.Message):
-        channel = await message.get_channel()
-        if message.content.startswith("$eval"):
-            if int(message.author.id) != 892080548342820925:
-                return await channel.send(
-                    "You must be the bot owner to use this command. Also, no."
+    @prefixed_command(name="eval")
+    async def _eval(self, ctx: PrefixedContext, *, code: str) -> None:
+        """Evaluates some code."""
+
+        if int(ctx.author.id) != 892080548342820925:
+            return await ctx.send(
+                "You must be the bot owner to use this command. Also, no."
+            )
+
+        env = {
+            "ctx": ctx,
+            "channel": ctx.channel,
+            "author": ctx.message.author,
+            "user": ctx.message.author,
+            "guild": ctx.guild,
+            "message": ctx.message,
+            "source": inspect.getsource,
+            "interactions": interactions,
+            "client": self.client,
+            "self.client": self.client,
+        }
+
+        env.update(globals())
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(code, "  ")}'
+        try:
+            exec(to_compile, env)
+        except Exception:
+            return await ctx.send(f"```py\n{traceback.format_exc()}\n```")
+
+        func = env["func"]
+        try:
+            with contextlib.redirect_stdout(stdout):
+                await func()
+        except Exception:
+            value = stdout.getvalue()
+            return await ctx.send(
+                f"```py\n{value}{traceback.format_exc()}\n```"
+            )
+        else:
+            value = stdout.getvalue()
+            if value and len(value) < 1001:
+                await ctx.send(f"```py\n{value}\n```")
+
+            elif value and len(value) > 0:
+                out_pages = page_paginator(value)
+                pag_pages = []
+                for page in out_pages:
+                    pag_pages.append(f"```py\n{page}\n```")
+
+                paginator = Paginator.create_from_list(
+                    self.client,
+                    content=pag_pages,
+                    page_size=2000,
                 )
 
-            ends = int(len(message.content) - 6)
-            code = str(message.content)[-ends:]
-
-            env = {
-                "ctx": channel,
-                "channel": channel,
-                "author": message.author,
-                "user": interactions.User(
-                    **await self.client._http.get_user(int(message.author.id)),
-                    _client=self.client._http,
-                ),
-                "guild": await message.get_guild(),
-                "message": message,
-                "source": inspect.getsource,
-                "interactions": interactions,
-                "client": self.client,
-                "self.client": self.client,
-            }
-
-            env.update(globals())
-            stdout = io.StringIO()
-
-            to_compile = f'async def func():\n{textwrap.indent(code, "  ")}'
-            try:
-                exec(to_compile, env)
-            except Exception:
-                return await channel.send(f"```py\n{traceback.format_exc()}\n```")
-
-            func = env["func"]
-            try:
-                with contextlib.redirect_stdout(stdout):
-                    await func()
-            except Exception:
-                value = stdout.getvalue()
-                return await channel.send(
-                    f"```py\n{value}{traceback.format_exc()}\n```"
-                )
+                await paginator.send(ctx)
             else:
-                value = stdout.getvalue()
-                if value and len(value) < 1001:
-                    await channel.send(f"```py\n{value}\n```")
-                elif value:
-                    file = io.StringIO(value)
-                    with file as f:
-                        file = interactions.File(filename="output.txt", fp=f)
-                        await channel.send(
-                            f"```py\n{value[:200]}...\n```",
-                            files=file,
-                        )
-                else:
-                    await channel.send("```py\nNone\n```")
+                await ctx.send("```py\nNone\n```", ephemeral=True)
 
-    @interactions.extension_command(
+    @interactions.slash_command(
         name="shell",
         description="Runs a shell command.",
         options=[
-            interactions.Option(
+            interactions.SlashCommandOption(
                 type=interactions.OptionType.STRING,
                 name="command",
                 description="The command to run.",
@@ -191,7 +204,7 @@ class Eval(interactions.Extension):
             )
         ],
     )
-    async def _shell(self, ctx: interactions.CommandContext, command: str):
+    async def shell(self, ctx: interactions.InteractionContext, command: str) -> None:
         """Runs a shell command."""
 
         await ctx.defer()
@@ -221,18 +234,16 @@ class Eval(interactions.Extension):
         pag_pages = []
         for page in out_pages:
             pag_pages.append(
-                Page(
-                    f"```sh\n$ {command}\n\n{page}\n\nReturn code {proc.returncode};\n```"
-                )
+                f"```sh\n$ {command}\n\n{page}\n\nReturn code {proc.returncode};\n```"
             )
-        await Paginator(
-            client=self.client,
-            ctx=ctx,
-            pages=pag_pages,
-            timeout=10,
-            use_select=False,
-            remove_after_timeout=True,
-        ).run()
+
+        paginator = Paginator.create_from_list(
+            self.client,
+            content=pag_pages,
+            page_size=2000,
+        )
+
+        await paginator.send(ctx)
 
 
 def setup(client) -> None:
