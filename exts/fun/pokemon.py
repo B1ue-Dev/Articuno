64e9bd2e3591bd29fb4e291a28f4cbff2441d992
B1ue-Dev <60958064+B1ue-Dev@users.noreply.github.com>
2023-03-17 01:45:23 +0700

    feat: Update pokemon.py to v5

--- exts/fun/pokemon.py
+++ exts/fun/pokemon.py
@@ -1,11 +1,9 @@
 """
-This module is for Pokemon commands.
+Pokemon related commands.
 
-(C) 2022 - Jimmy-Blue
+(C) 2022-2023 - B1ue-Dev
 """
 
-import logging
-import datetime
 import json
 import interactions
 
@@ -16,11 +14,11 @@ class Pokemon(interactions.Extension):
     def __init__(self, client: interactions.Client) -> None:
         self.client: interactions.Client = client
 
-    @interactions.extension_command(
+    @interactions.slash_command(
         name="pokedex",
         description="Shows the information about a Pokemon.",
         options=[
-            interactions.Option(
+            interactions.SlashCommandOption(
                 type=interactions.OptionType.STRING,
                 name="pokemon_name",
                 description="Pokemon name",
@@ -29,7 +27,9 @@ class Pokemon(interactions.Extension):
             ),
         ],
     )
-    async def _pokedex(self, ctx: interactions.CommandContext, pokemon_name: str):
+    async def pokedex(
+        self, ctx: interactions.SlashContext, pokemon_name: str
+    ) -> None:
         """Shows the information about a Pokemon."""
 
         name_lower = pokemon_name.lower()
@@ -75,7 +75,7 @@ class Pokemon(interactions.Extension):
                 text=f"First introduced in Generation {db[name_lower]['generation']}",
                 icon_url="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png",
             )
-            thumbnail = interactions.EmbedImageStruct(url=sprites_url_still)
+            thumbnail = interactions.EmbedAttachment(url=sprites_url_still)
             fields = [
                 interactions.EmbedField(
                     name="Information",
@@ -91,7 +91,9 @@ class Pokemon(interactions.Extension):
                     ),
                     inline=True,
                 ),
-                interactions.EmbedField(name="Stats", value=stats, inline=True),
+                interactions.EmbedField(
+                    name="Stats", value=stats, inline=True
+                ),
             ]
             embed = interactions.Embed(
                 title=f"{name}",
@@ -105,23 +107,29 @@ class Pokemon(interactions.Extension):
 
             await ctx.send(embeds=embed)
 
-    @interactions.extension_autocomplete(command="pokedex", name="pokemon_name")
-    async def auto_complete(
-        self, ctx: interactions.CommandContext, pokemon_name: str = ""
-    ):
+    @pokedex.autocomplete(option_name="pokemon_name")
+    async def pokedex_autocomplete(
+        self, ctx: interactions.AutocompleteContext
+    ) -> None:
+        """Autocomplete for /pokedex."""
+
+        pokemon_name: str = ctx.input_text
         if pokemon_name != "":
             letters: list = pokemon_name
         else:
             letters = []
-        data = json.loads(open("./db/pokemon.json", "r", encoding="utf8").read())
+        data: list[str] = json.loads(
+            open("./db/pokemon.json", "r", encoding="utf8").read()
+        )
         if len(pokemon_name) == 0:
-            await ctx.populate(
+            await ctx.send(
                 [
-                    interactions.Choice(
-                        name=name[0].capitalize(), value=name[0].capitalize()
-                    )
+                    {
+                        "name": name[0].capitalize(),
+                        "value": name[0].capitalize(),
+                    }
                     for name in (
-                        list(data.items())[0:9]
+                        list(data.items())[0:24]
                         if len(data) > 10
                         else list(data.items())
                     )
@@ -133,38 +141,43 @@ class Pokemon(interactions.Extension):
                 focus: str = "".join(letters)
                 if focus.lower() in pkmn_name and len(choices) < 20:
                     choices.append(
-                        interactions.Choice(
-                            name=pkmn_name.capitalize(), value=pkmn_name.capitalize()
-                        )
+                        {
+                            "name": pkmn_name.capitalize(),
+                            "value": pkmn_name.capitalize(),
+                        }
                     )
-            await ctx.populate(choices)
+            await ctx.send(choices)
 
-    @interactions.extension_listener(name="on_message_create")
-    async def message_create(self, message: interactions.Message):
-        channel = await message.get_channel()
-        if message.content.startswith("$shiny"):
-            ends = int(len(message.content) - 7)
-            msg = str(message.content)[-ends:].lower()
-            data = json.loads(open("./db/pokemon.json", "r").read())
+    @interactions.listen()
+    async def on_message_create(
+        self, message: interactions.events.MessageCreate
+    ) -> None:
+        """Pokemon sprites related."""
+
+        _msg: interactions.Message = message.message
+
+        if _msg.content.startswith("$shiny"):
+            ends = int(len(_msg.content) - 7)
+            msg = str(_msg.content)[-ends:].lower()
+            data = json.loads(
+                open("./db/pokemon.json", "r", encoding="utf8").read()
+            )
             if msg in data:
                 img = f"https://play.pokemonshowdown.com/sprites/ani-shiny/{msg}.gif"
-                embed = interactions.Embed(image=interactions.EmbedImageStruct(url=img))
-                await channel.send(embeds=embed)
+                embed = interactions.Embed(
+                    images=[interactions.EmbedAttachment(url=img)]
+                )
+                await _msg.channel.send(embeds=embed)
 
-        elif message.content.startswith("$"):
-            ends = int(len(message.content) - 1)
-            msg = str(message.content)[-ends:].lower()
-            data = json.loads(open("./db/pokemon.json", "r").read())
+        elif _msg.content.startswith("$"):
+            ends = int(len(_msg.content) - 1)
+            msg = str(_msg.content)[-ends:].lower()
+            data = json.loads(
+                open("./db/pokemon.json", "r", encoding="utf8").read()
+            )
             if msg in data:
                 img = f"https://play.pokemonshowdown.com/sprites/ani/{msg}.gif"
-                embed = interactions.Embed(image=interactions.EmbedImageStruct(url=img))
-                await channel.send(embeds=embed)
-
-
-def setup(client) -> None:
-    """Setup the extension."""
-    log_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=7)).strftime(
-        "%d/%m/%Y %H:%M:%S"
-    )
-    Pokemon(client)
-    logging.debug("""[%s] Loaded Pokemon extension.""", log_time)
+                embed = interactions.Embed(
+                    images=[interactions.EmbedAttachment(url=img)]
+                )
+                await _msg.channel.send(embeds=embed)
