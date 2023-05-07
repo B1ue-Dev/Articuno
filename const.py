01414f1dcf67372e0a6e6cca85b74687979102ba
Blue <60958064+B1ue-Dev@users.noreply.github.com>
2023-05-07 20:05:27 +0000

    chore: Update bot version to v5.0.2

--- const.py
+++ const.py
@@ -12,7 +12,7 @@ dotenv.load_dotenv()
 TOKEN = os.getenv("TOKEN")
 """The token of the bot."""
 
-VERSION = "v5.0.1"
+VERSION = "v5.0.2"
 """Bot version."""
 
 LOG_CHANNEL = os.getenv("LOG_CHANNEL")
