7e6b3ae4c139055ca8c7517cd84ac0664aa05252
B1ue-Dev <60958064+B1ue-Dev@users.noreply.github.com>
2023-07-10 00:07:22 +0700

    feat: 2 players mode tic tac toe

--- exts/fun/tictactoe.py
+++ exts/fun/tictactoe.py
@@ -17,9 +17,15 @@ class GameState(enum.IntEnum):
     ai = +1
 
 
+def remove_id_format(text: str) -> int:
+    """Remove the symbol arround a Discord ID string."""
+
+    return int(text.replace("<", "").replace(">", "").replace("@", ""))
+
+
 def render_board(board: list, disable=False) -> list:
     """
-    Converts the test_board into a visual representation using discord components
+    Converts the test_board into a visual representation using discord components.
     :param board: The game test_board
     :param disable: Disable the buttons on the test_board
     :return: List[action-rows]
@@ -30,15 +36,51 @@ def render_board(board: list, disable=False) -> list:
         for x in range(3):
             if board[i][x] == GameState.empty:
                 style = interactions.ButtonStyle.GRAY
+                label = "‎"
             elif board[i][x] == GameState.player:
                 style = interactions.ButtonStyle.PRIMARY
+                label = "x"
             else:
                 style = interactions.ButtonStyle.RED
+                label = "o"
             buttons.append(
                 interactions.Button(
                     style=style,
-                    label="‎",
+                    label=label,
                     custom_id=f"tic_tac_toe_button||{i},{x}",
+                    disabled=disable,
+                )
+            )
+    return interactions.spread_to_rows(*buttons, max_in_row=3)
+
+
+def alternate_render_board(board: list, disable=False) -> list:
+    """
+    Converts the test_board into a visual representation using discord components.
+    This is used for the 2nd player mode since I have not found the way to combine
+    everything into one callback yet.
+    :param board: The game test_board
+    :param disable: Disable the buttons on the test_board
+    :return: List[action-rows]
+    """
+
+    buttons = []
+    for i in range(3):
+        for x in range(3):
+            if board[i][x] == GameState.empty:
+                style = interactions.ButtonStyle.GRAY
+                label = "‎"
+            elif board[i][x] == GameState.player:
+                style = interactions.ButtonStyle.PRIMARY
+                label = "x"
+            else:
+                style = interactions.ButtonStyle.RED
+                label = "o"
+            buttons.append(
+                interactions.Button(
+                    style=style,
+                    label=label,
+                    custom_id=f"tic_tac_toe_2p||{i},{x}",
                     disabled=disable,
                 )
             )
@@ -179,6 +221,172 @@ class TicTacToe(interactions.Extension):
             components=render_board(copy.deepcopy(BoardTemplate)),
         )
 
+    @hybrid_slash_subcommand(
+        base="tictactoe",
+        base_description="TicTacToe command.",
+        name="2player",
+        description="Play TicTacToe in 2 players mode.",
+    )
+    @interactions.slash_option(
+        name="user",
+        description="The user you want to play against.",
+        opt_type=interactions.OptionType.USER,
+        required=True,
+    )
+    async def ttt_2p(
+        self, ctx: HybridContext, user: interactions.User
+    ) -> None:
+        """Play TicTacToee in 2 players mode."""
+
+        if int(ctx.user.id) == int(user.id):
+            return await ctx.send("You cannot challenge yourself.")
+
+        if int(user.id) == int(self.client.user.id):
+            return await ctx.send(
+                "To challenge me, do `/tictactoe easy` or `/tictactoe hard` instead."
+            )
+
+        accept_deny = [
+            interactions.ActionRow(
+                interactions.Button(
+                    style=interactions.ButtonStyle.SUCCESS,
+                    label="Accept",
+                    custom_id="accept",
+                ),
+                interactions.Button(
+                    style=interactions.ButtonStyle.DANGER,
+                    label="Deny",
+                    custom_id="deny",
+                ),
+            )
+        ]
+
+        msg = await ctx.send(
+            content=f"{ctx.user.mention} challenged {user.mention}.",
+            components=accept_deny,
+            allowed_mentions={"users": [int(user.id)]},
+        )
+
+        opn: interactions.Member = None
+        async for _user in msg.mention_users:
+            if int(_user.id) != int(ctx.user.id):
+                opn = _user
+
+        try:
+
+            def _check(_ctx: interactions.ComponentContext) -> bool:
+                return int(_ctx.ctx.user.id) == int(opn.user.id) and int(
+                    _ctx.ctx.channel_id
+                ) == int(ctx.channel_id)
+
+            res = await self.client.wait_for_component(
+                components=accept_deny,
+                messages=int(msg.id),
+                check=_check,
+                timeout=15,
+            )
+
+            if res.ctx.custom_id == "accept":
+                goes_first: interactions.Member = random.choice(
+                    [opn.user, ctx.user]
+                )
+                await res.ctx.edit_origin(
+                    content="".join(
+                        [
+                            f"{ctx.author.mention} **VS** {opn.mention} tic tac toe game.\n",
+                            f"<@{goes_first.id}> turn.",
+                        ]
+                    ),
+                    components=alternate_render_board(
+                        copy.deepcopy(BoardTemplate)
+                    ),
+                )
+
+            elif res.ctx.custom_id == "deny":
+                await res.ctx.edit_origin(
+                    content="Challenge cancelled.",
+                    components=[],
+                )
+
+        except asyncio.TimeoutError:
+            pass
+
+    @interactions.component_callback(
+        interactions.get_components_ids(
+            alternate_render_board(board=copy.deepcopy(BoardTemplate))
+        )
+    )
+    async def alternate_process_turn(
+        self, ctx: interactions.ComponentContext
+    ) -> None:
+        og = remove_id_format(ctx.message.content.split()[0])
+        chd = remove_id_format(ctx.message.content.split()[2])
+
+        await ctx.defer(edit_origin=True)
+        player1 = remove_id_format(ctx.message.content.split()[-2])
+        player2 = og if og != player1 else chd
+
+        if int(ctx.user.id) not in [player1, player2]:
+            return
+
+        if int(ctx.user.id) != player1:
+            return
+
+        button_pos = (ctx.custom_id.split("||")[-1]).split(",")
+        button_pos = [int(button_pos[0]), int(button_pos[1])]
+        components = ctx.message.components
+
+        board = board_state(components)
+
+        # We will reuse the GameState object.
+        # Original challenger will be assigned as .player
+        # Challenged user will be assigned as .ai
+        if board[button_pos[0]][button_pos[1]] == GameState.empty:
+            if og == int(ctx.user.id):
+                board[button_pos[0]][button_pos[1]] = GameState.player
+            elif chd == int(ctx.user.id):
+                board[button_pos[0]][button_pos[1]] = GameState.ai
+        else:
+            return
+
+        if win_state(board, GameState.player):
+            winner = og
+        elif win_state(board, GameState.ai):
+            winner = chd
+        elif len(get_possible_positions(board)) == 0:
+            winner = "Nobody"
+        else:
+            winner = None
+
+        if winner == "Nobody":
+            content = f"Nobody has won!"
+        elif winner in [og, chd]:
+            content = f"<@{winner}> is the winner!"
+        else:
+            content = "".join(
+                [
+                    f"<@{og}> **VS** <@{chd}> tic tac toe game.\n",
+                    f"<@{player2}> turn.",
+                ]
+            )
+
+        allowed_mentions = None
+        if winner is not None:
+            if isinstance(winner, int):
+                allowed_mentions = {"users": [winner]}
+            else:
+                allowed_mentions = None
+        else:
+            allowed_mentions = None
+
+        await ctx.edit_origin(
+            content=content,
+            components=alternate_render_board(
+                board, disable=winner is not None
+            ),
+            allowed_mentions=allowed_mentions,
+        )
+
     @interactions.component_callback(
         interactions.get_components_ids(
             render_board(board=copy.deepcopy(BoardTemplate))
@@ -205,7 +413,6 @@ class TicTacToe(interactions.Extension):
         # Articuno will get all possible moves, and use the random
         # module to get a position.
         if ctx.message.content.find("easy mode") != -1:
-            print("Easy")
             if board[button_pos[0]][button_pos[1]] == GameState.empty:
                 board[button_pos[0]][button_pos[1]] = GameState.player
                 if not win_state(board, GameState.player):
@@ -220,7 +427,6 @@ class TicTacToe(interactions.Extension):
         # Articuno will use minimax to make sure that the game will
         # never be a win for the player.
         elif ctx.message.content.find("hard mode") != -1:
-            print("Hard")
             if board[button_pos[0]][button_pos[1]] == GameState.empty:
                 board[button_pos[0]][button_pos[1]] = GameState.player
                 if not win_state(board, GameState.player):
