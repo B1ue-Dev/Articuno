176b71327232c565ea870ded212363b8cfac9d19
B1ue-Dev <60958064+B1ue-Dev@users.noreply.github.com>
2023-08-16 23:36:10 +0700

    refactor: Rewrite the message and link

--- src/exts/core/author.py
+++ src/exts/core/author.py
@@ -18,6 +18,9 @@ class Author(interactions.Extension):
 
     def __init__(self, client: interactions.Client) -> None:
         self.client: interactions.Client = client
+        self.topgg: str = "https://top.gg/bot/809084067446259722"
+        self.github: str = "https://github.com/B1ue-Dev/Articuno"
+        self.invite: str = "https://discord.com/oauth2/authorize?client_id=809084067446259722&permissions=1644905889023&scope=bot%20applications.commands"
 
     @hybrid_slash_command(
         name="about",
@@ -30,12 +33,12 @@ class Author(interactions.Extension):
             interactions.Button(
                 style=interactions.ButtonStyle.LINK,
                 label="GitHub",
-                url="https://github.com/B1ue-Dev/Articuno",
+                url=self.github,
             ),
             interactions.Button(
                 style=interactions.ButtonStyle.LINK,
                 label="Top.gg",
-                url="https://top.gg/bot/809084067446259722",
+                url=self.topgg,
             ),
         ]
 
@@ -43,14 +46,19 @@ class Author(interactions.Extension):
             title="About Articuno",
             description="".join(
                 [
-                    "Articuno is a multi-purpose Discord Bot that can do a wide range of jobs, mostly ",
-                    "with different fun commands. Highlight commands, such as `/img`, `/tag`, `/emoji`, `whos_that_pokemon`,",
-                    " etc. The goal of Articuno is to make your server a better place, with different misc commands,",
-                    " moderating your server with simple manners, and a built-in log system that requires simple ",
-                    " setup.\n\nArticuno is a side-project that I work on, so commands can be broken sometimes. If",
-                    " you encounter any issue, be sure to use `/invite` and join the support server to report the ",
-                    "problem.\n\nWhenever that is creating tags with autocomplete, bringing joys to members, moderating",
-                    " the server, Articuno has you covered.",
+                    "Articuno is a multi-purpose Discord Bot that can do a ",
+                    "wide range of jobs, mostly with different fun commands.",
+                    " Highlight commands, such as `/img`, `/tag`, `/emoji`, ",
+                    "`whos_that_pokemon`, etc. The goal of Articuno is to ",
+                    "make your server a better place, with different misc ",
+                    "commands, moderating your server with simple manners, ",
+                    "and a built-in log system that requires simple setup.",
+                    "\n\nArticuno is a side-project that I work on, so ",
+                    "commands can be broken sometimes. If you encounter any ",
+                    "issue, be sure to use `/invite` and join the support ",
+                    "server to report the problem.\n\nWhenever creating tags",
+                    " with autocomplete, bringing joys to members, ",
+                    "moderating the server, Articuno has you covered.",
                 ]
             ),
             color=0x7CB7D3,
@@ -80,7 +88,7 @@ class Author(interactions.Extension):
         )
         embed = interactions.Embed(
             title="Credits",
-            description="Articuno is being maintained, developed and improved by Blue#2095.",
+            description="Articuno is being maintained, developed and improved by @b1uedev.",
             color=0x7CB7D3,
             footer=footer,
         )
@@ -98,7 +106,7 @@ class Author(interactions.Extension):
             interactions.Button(
                 style=interactions.ButtonStyle.LINK,
                 label="Add me to your server",
-                url="https://discord.com/oauth2/authorize?client_id=809084067446259722&permissions=1644905889023&scope=bot%20applications.commands",
+                url=self.invite,
             ),
             interactions.Button(
                 style=interactions.ButtonStyle.LINK,
@@ -108,7 +116,7 @@ class Author(interactions.Extension):
             interactions.Button(
                 style=interactions.ButtonStyle.LINK,
                 label="Vote me on Top.gg",
-                url="https://top.gg/bot/809084067446259722/vote",
+                url=f"{self.topgg}/vote",
             ),
         ]
 
@@ -118,7 +126,12 @@ class Author(interactions.Extension):
         )
         embed = interactions.Embed(
             title="Invite Articuno to your server",
-            description="Click the button below to invite Articuno to your server.\n\nIf you have any questions, feel free to join the support server.",
+            description="".join(
+                [
+                    "Click the button below to invite Articuno to your server.",
+                    "\n\nIf you have any questions, feel free to join the support server.",
+                ]
+            ),
             color=0x7CB7D3,
             footer=footer,
         )
